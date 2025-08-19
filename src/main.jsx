c2e70aeb2c7344b56db2ade5f6e4e1fe183d0445
Amite55 <amite403@gmail.com>
2025-08-19 16:00:19 +0600

    added new branch wish grocery

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,5 @@ import 'antd/dist/reset.css';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
-    {/* ----------------- hare is root layout ----------------- */}
   </StrictMode>,
 )
