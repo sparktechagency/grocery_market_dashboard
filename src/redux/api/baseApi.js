c6429fa94e1402d9727d33e3e74eed4ddbdc1528
almas hossain <almashossain7384@gmail.com>
2026-02-23 14:44:04 +0600

    base url changes

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-  baseUrl: "http://103.186.20.114:8001/api", // live url
+  baseUrl: "http://api.100.22.57.247.nip.io/api", // live url
     // baseUrl: "http://103.186.20.114:8009/api",
     prepareHeaders: (headers, { getState }) => {
       const token = localStorage.getItem("token");
